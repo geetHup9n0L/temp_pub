b6975d7820e45e6fc2a1b10086a75438f4e4bdce
Lê Minh Long <lmlong1205@gmail.com>
2026-06-02 13:04:17 +0700

    Create build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,12 @@
+dependencies {
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
+}
